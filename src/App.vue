56ab3d1e282a1581930a0f2b156fd86d1f75ba14
francisco-JGC <jjjchico1@gmail.com>
2025-03-07 15:16:00 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { computed } from "vue";
+
+const title = "que onda perro estoy aprendiendo vue.js";
+const onClick = () => alert("Hola");
+
+computed: {
+  activeUser: () => {};
+}
+</script>
+
+<template>
+  <div class="title">
+    {{ title }}
+    <button v-on:click="onClick()">Click me</button>
+  </div>
+</template>
+
+<style scoped>
+.title {
+  min-height: 100dvh;
+}
+</style>
